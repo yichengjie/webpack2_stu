21ba663cb854d60b4599a2c3d3d4cd191a692133
yichengjie <626659321@qq.com>
2017-06-20 16:48:40 +0800

    优化配置

--- price3/config/dev.js
+++ price3/config/dev.js
@@ -2,9 +2,10 @@ const webpackMerge = require('webpack-merge');
 const commonConfig = require('./base.js');
 let path = require('path') ;
 let common = require('./common.js') ;
+let localIPAddress = common.getIPAdress() ;
+let serverIPAddress = localIPAddress;
 
 module.exports = function() {
-   let ipAddress= common.getIPAdress() ;
     return webpackMerge(commonConfig(), {
         devtool: 'cheap-module-eval-source-map',
         output: {
@@ -16,12 +17,12 @@ module.exports = function() {
             port: 3000,
             //所有来自 dist/目录的文件都做 gzip 压缩和提供为服务
             compress:true,
-            host:ipAddress,
+            host:localIPAddress,
             contentBase: [path.resolve(__dirname, '../public')],  // New
             //其实很简单的，只要配置这个参数就可以了  
             proxy: {  
                 '/api/*': {  
-                    target: 'http://'+ipAddress+':8080/',  
+                    target: 'http://'+serverIPAddress+':8080/',  
                     secure: false  
                 }  
             }  
